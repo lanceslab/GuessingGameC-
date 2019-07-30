7634516b342f7e9ea6c1c189184b6866e0c7a2c7
LancesLab <lanceandsheri@yahoo.com>
2019-07-30 15:17:44 -0400

    Changed background color

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Enter a number and I\'ll see how many tries it take me te Guess it!"});
+            this.listBox1.Location = new System.Drawing.Point(0, 48);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(529, 356);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(373, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter number here";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 404);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "   Lance Taylor\'s         Guessing Game !!!!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+void searchGuess() {
+	int i, guessCount = 0, guess = 0, initialNumber = 1, maxNumber = 1000;
+    bool isCorrect = false;
+    int tNumber = Convert.ToInt32(textBox1.Text);    
+    do
+    {
+        guess = getRandomInt(initialNumber, maxNumber);
+        guessCount++;
+        if(guess == tNumber) {
+            isCorrect = true;
+            listBox1.Items.Add("I guessed the correct number in " + guessCount + " tries!");
+        }
+        else if ( tNumber > guess ) {
+            initialNumber = guess;
+            guessCount++;
+            //listBox1.Items.Add("I guessed HIGH.." + guess + " I'll try again ");
+            listBox1.Items.Add("I guessed LOW.." + guess + " I'll try again ");
+         }
+        else if ( tNumber < guess ) {
+            maxNumber = guess;
+			guessCount++;
+            //listBox1.Items.Add("I guessed LOW.." + guess + " I'll try again ");
+            listBox1.Items.Add("I guessed HIGH.." + guess + " I'll try again ");
+         }        
+    } while (isCorrect == false);
+}
+
+        
+int getRandomInt(int min, int max) {
+    Random rnd = new Random();
+    int month = rnd.Next(min, max);
+    return month;// Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
+
+
+
+
+
+        //textBox1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            searchGuess();
+        }
+    }
+}
